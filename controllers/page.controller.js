dad6b8b08adc6a7b9a254e4b080bbc25e5ce0991
Mikhail <mikhailkamal00@gmail.com>
2025-01-06 15:23:30 +0800

    add page functionality

--- controllers/page.controller.js
+++ controllers/page.controller.js
@@ -1,16 +1,55 @@
-const getHomeHandler = (req, res) => {
-  // Need to render the home page, need to get all of the books available in the db.
+const asyncHandler = require("express-async-handler");
+const { getAverageRatings } = require("./books.controller");
+const { findManyBooks, findBookById } = require("../services/book.services");
+const { findManyReviews } = require("../services/review.services");
 
-  // Render home page
-  res.render("../views/index.ejs"); // Still need to add the data for the list of books
-};
+const getHomePageHandler = asyncHandler(async (req, res) => {
+  const books = await findManyBooks();
+
+  try {
+    const plainBooks = books.map((book) => book.toJSON());
+    return res.render("../views/index.ejs", { data: plainBooks });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      location: "controllers/page/getHomePageHandler",
+      message: "Internal server error.",
+      error: error,
+    });
+  }
+});
+
+const getSpecificBookPageHandler = asyncHandler(async (req, res) => {
+  const bookId = req.params.id;
+  try {
+    let book = await findBookById(bookId);
+    book = book.toJSON();
+
+    const bookRating = await getAverageRatings(bookId);
+    const reviews = await findManyReviews({ bookId: bookId });
+    const plainReview = reviews.map((review) => review.toJSON());
+
+    return res.render("book.ejs", {
+      book: book,
+      reviews: plainReview,
+      rating: bookRating,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      location: "controllers/page/getSpecificBookPageHandler",
+      message: "Internal server error.",
+      error: error,
+    });
+  }
+});
 
 const getAddBookPageHandler = (req, res) => {
-  res.render("../views/add.ejs")
-}
+  res.render("../views/add.ejs");
+};
 
-// const getAddBookPageHandler = (req, res) => {
-//   res.render("../views/add.ejs")
-// }
-
-module.exports = { getHomeHandler, getAddBookPageHandler };
+module.exports = {
+  getHomePageHandler,
+  getAddBookPageHandler,
+  getSpecificBookPageHandler,
+};

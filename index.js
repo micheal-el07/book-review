c2fd0693cf84e50381af07d801fad31d8b72ec7a
Mikhail <mikhailkamal00@gmail.com>
2025-01-06 15:21:11 +0800

    clean up index js

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const { router } = require("./routes");
-const errorHandler = require("./middleware/error.middleware");
 const { PORT } = require("./config/env.config");
 const { db } = require("./models");
+const errorHandler = require("./middleware/error.middleware");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/", router);
 
+// Error handling middleware
 app.use(errorHandler);
 
+// Send all invalid request endpoint to specific error page
 app.all("*", function (req, res) {
-  // res.status(404).json({ status: "failed", message: "Invalid endpoint!" });
   res.status(404).render("../views/error-page.ejs", {
     result: {
       status: "Invalid endpoint.",

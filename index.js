85a5bce62da7ac2dbf2a46b4a90c61903b5d672b
Mikhail <mikhailkamal00@gmail.com>
2025-01-07 17:43:51 +0800

    add upload to public path file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

81219c94cb1e9729def04291e8b05da8911704b6
Mikhail <mikhailkamal00@gmail.com>
2025-01-07 17:41:56 +0800

    add upload picture middleware

---
+++ middleware/upload.middleware.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+const path = require("path");
+
+// Configure storage for uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads")); // Save in 'uploads' folder
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// File filter to allow only images
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type, only images are allowed!"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload.single("bookCover");
